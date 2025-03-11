525075e90a934024a10252cd34b0e59b93060712
sinobata <tabataryo@team-lab.com>
2025-03-11 14:00:37 +0900

    feat: liffの関数を追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,9 @@ import Head from "next/head";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import liff from '@line/liff';
+import { useState } from "react";
+import { set } from "@liff/store";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,6 +17,36 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
+  const [userInfo, setUserInfo] = useState({
+    lang: "",
+    version: "",
+    isInClient: false,
+    isLoggedIn: false,
+    os: "",
+    lineVersion: "",
+  });
+
+  const handleCloseButton = () => {
+    if (!liff.isInClient()) {
+      window.alert(
+        "This button is unavailable as LIFF is currently being opened in an external browser."
+      );
+    } else {
+      liff.closeWindow();
+    }
+  }
+
+  const handleOpenExternalButton = () => {
+    setUserInfo({
+      lang: liff.getAppLanguage(),
+      version: liff.getVersion(),
+      isInClient: liff.isInClient(),
+      isLoggedIn: liff.isLoggedIn(),
+      os: liff.getOS() || '',
+      lineVersion: liff.getLineVersion() || '',
+    });
+  }
+
   return (
     <>
       <Head>
@@ -26,91 +59,15 @@ export default function Home() {
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
       >
         <main className={styles.main}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js logo"
-            width={180}
-            height={38}
-            priority
-          />
-          <ol>
-            <li>
-              Get started by editing <code>src/pages/index.tsx</code>.
-            </li>
-            <li>Save and see your changes instantly.</li>
-          </ol>
-
+          <div>LIFF APP</div>
           <div className={styles.ctas}>
-            <a
-              className={styles.primary}
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                className={styles.logo}
-                src="/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
-              />
-              Deploy now
-            </a>
-            <a
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.secondary}
-            >
-              Read our docs
-            </a>
+            <button className={styles.primary} onClick={handleCloseButton}>Close Window</button>
+            <button className={styles.secondary} onClick={handleOpenExternalButton}>情報取得</button>
+          </div>
+          <div>
+            <pre>{JSON.stringify(userInfo, null, 2)}</pre>
           </div>
         </main>
-        <footer className={styles.footer}>
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/file.svg"
-              alt="File icon"
-              width={16}
-              height={16}
-            />
-            Learn
-          </a>
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/window.svg"
-              alt="Window icon"
-              width={16}
-              height={16}
-            />
-            Examples
-          </a>
-          <a
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/globe.svg"
-              alt="Globe icon"
-              width={16}
-              height={16}
-            />
-            Go to nextjs.org →
-          </a>
-        </footer>
       </div>
     </>
   );
