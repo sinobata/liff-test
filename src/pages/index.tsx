bdfe813317cc25419013d70d7d1ceb9763bc621e
sinobata <tabataryo@team-lab.com>
2025-03-11 14:01:53 +0900

    fix: build error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import liff from '@line/liff';
 import { useState } from "react";
-import { set } from "@liff/store";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
