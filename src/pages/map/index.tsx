dec26b000d8baf2d85fc144aeacb082f17ed2cce
sinobata <tabataryo@team-lab.com>
2025-03-11 16:53:39 +0900

    fix: map調整

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -1,65 +1,62 @@
-import { useEffect, useState } from 'react';
-import liff from '@line/liff';
-import { LatLngExpression } from 'leaflet';
+import React, { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
+import 'leaflet/dist/leaflet.css';
 
 const MapContainer = dynamic(() => import('react-leaflet').then(mod => mod.MapContainer), { ssr: false });
 const TileLayer = dynamic(() => import('react-leaflet').then(mod => mod.TileLayer), { ssr: false });
 const Marker = dynamic(() => import('react-leaflet').then(mod => mod.Marker), { ssr: false });
 const Popup = dynamic(() => import('react-leaflet').then(mod => mod.Popup), { ssr: false });
 
-
-export default function  Map() {
-  const [location, setLocation] = useState<LatLngExpression | null>(null);
-  const [error, setError] = useState<string | null>(null);
+export default function Map() {
+  const [position, setPosition] = useState<[number, number] | null>(null);
+  const [customIcon, setCustomIcon] = useState<L.Icon | null>(null);
 
   useEffect(() => {
-    if(typeof window === 'undefined') {
-      return;
-    }
-    const initializeLiff = async () => {
-      try {
-        if (!liff.isLoggedIn()) {
-          liff.login();
-        }
+    if (typeof window !== 'undefined') {
+      const L = require('leaflet');
+
+      // カスタムアイコンの作成
+      const icon = L.icon({
+        iconUrl: '/marker.svg', // カスタムアイコンのパス
+        iconSize: [40, 41], // アイコンのサイズ
+        iconAnchor: [12, 41], // アイコンのアンカー位置
+        popupAnchor: [1, -34], // ポップアップのアンカー位置
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png', // 影の画像
+        shadowSize: [41, 41], // 影のサイズ
+      });
+
+      setCustomIcon(icon);
+
+      if ('geolocation' in navigator) {
         navigator.geolocation.getCurrentPosition(
           (position) => {
-            setLocation([position.coords.latitude, position.coords.longitude]);
+            setPosition([position.coords.latitude, position.coords.longitude]);
           },
           (error) => {
-            console.error(error);
-            setError('Failed to get current position');
+            console.error('Error obtaining geolocation', error);
           }
         );
-      } catch (error) {
-        console.error(error);
-        setError('LIFF initialization failed');
+      } else {
+        console.error('Geolocation is not available');
       }
-    };
-    initializeLiff();
+    }
   }, []);
 
-  if (error) {
-    return <div>{error}</div>;
-  }
-
   return (
-    <div>
-      {location ? (
-        <MapContainer center={location} zoom={15} style={{ height: '100%', width: '100%' }}>
+    <div style={{ height: '100vh' }}>
+      {position && customIcon ? (
+        <MapContainer center={position} zoom={16} style={{ height: '100%', width: '100%' }}>
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           />
-          <Marker position={location}>
-            <Popup>
-              You are here.
-            </Popup>
+          <Marker position={position} icon={customIcon}>
+            <Popup>Your location</Popup>
           </Marker>
         </MapContainer>
       ) : (
-        <div>Loading...</div>
+        <p>Loading map...</p>
       )}
     </div>
   );
-};
+}
