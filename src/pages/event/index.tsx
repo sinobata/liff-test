5498ccb1f4cec0c183561649e1dd883419b23ea6
sinobata <tabataryo@team-lab.com>
2025-03-12 12:11:11 +0900

    fix: 画像のサイズ調整

--- src/pages/event/index.tsx
+++ src/pages/event/index.tsx
@@ -8,7 +8,7 @@ export default function Event() {
       <h1>
         イベント詳細
       </h1>
-      <Image src="/qr.png" alt="イベント画像" width={150} height={150} />
+      <Image src="/qr.png" alt="イベント画像" width={150} height={150} style={{width: '100%', height: '100%'}} />
     </>
   );
 }
